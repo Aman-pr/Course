3cc810f5d68181c7c1a3d5bef6804775e1e8c2de
Aman-pr <amankhan2729@gmail.com>
2025-10-04 06:18:48 +0000

    Day_1

---
+++ BASH/1.day/drill/1.day.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+    1  ls
+    2  cd BASH/
+    3  ls
+    4  mkdir 1.day
+    5  cd mkdir
+    6  cd 1.day/
+    7  ls
+    8  mkdir drill
+    9  cd drill/
+   10  ls
+   11  mkdir playground
+   12  touch 1.file.txt 2.file.txt 3.file.txt
+   13  ls
+   14  mkdir sub1 sub2
+   15  cp 1.file.txt sub1/1.file.txt
+   16  mv 2.file.txt sub2/2.file.txt
+   17  rm 3.file.txt 
+   18  ls -R
+
+
+
