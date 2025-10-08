bdb9e08b20b0ce057944eaf586db07583418e41b
Aman-pr <amankhan2729@gmail.com>
2025-10-08 23:42:10 +0530

    Add files via upload

---
+++ Lang_chain_campusX/Chain/Runnable/seq_Runnables.py
@@ -0,0 +1,36 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableSequence
+import os
+load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
+if not api_key:
+    print("GROQ_API_KEY environment variable not set")   
+else:
+    print("GROQ_API_KEY environment variable found")
+
+#Prompt template
+template = PromptTemplate(
+    template="Explain the following in simple terms: {text}",
+    input_variables=["text"]
+)
+
+template_2 = PromptTemplate(
+    template="Explain the following in 5 lines: {text}",
+    input_variables=["text"]
+)
+
+#Output parser
+output_parser = StrOutputParser()
+
+#Model
+llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+#Runnable sequence
+runnable = RunnableSequence(
+    template,llm,output_parser,llm,template_2
+)
+result=runnable.invoke({"text":"Nividia vs AMD"})
+print(result)
