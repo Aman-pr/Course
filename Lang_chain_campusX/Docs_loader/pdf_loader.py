4c83c3907e9627d8f23fb75538bc4e9e62996f1b
Aman-pr <amankhan2729@gmail.com>
2026-01-25 14:15:54 +0530

    Add files via upload

---
+++ Lang_chain_campusX/Docs_loader/pdf_loader.py
@@ -0,0 +1,34 @@
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("GROQ_API_KEY")
+if not API_KEY:
+    print("GROQ_API_KEY not set")
+else:
+    print("Working ")
+model = ChatGroq(model="llama-3.3-70b-versatile")
+
+prompt=PromptTemplate(
+    template="The summery of this documents {text}",
+    input_variables=['text']
+)
+
+parser=StrOutputParser()
+
+loader=PyPDFLoader('wadhaawani.pdf')
+
+docs=loader.load()
+print(type(docs))
+print(len(docs))
+print(docs[0].page_content)
+print(docs[0].metadata)
+
+chain=prompt|model|parser
+
+print(chain.invoke({'text':docs[0].page_content}))
