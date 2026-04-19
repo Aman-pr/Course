eca08be04d7930fdc96a39a85168805bc7217e07
Aman-pr <amankhan2729@gmail.com>
2026-04-19 20:14:28 +0530

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,62 @@
+from  langgraph.graph import StateGraph ,START,END
+from typing import TypedDict
+
+class bmi_details(TypedDict):
+    weight:float
+    height:float
+    bmi:float 
+    category:str
+
+
+
+def main(bmi:bmi_details)->bmi_details:
+    height=bmi['height']/100
+    weight=bmi['weight']
+    bmi_calculated=weight/(height**2)
+
+    bmi['bmi']=bmi_calculated
+
+    return bmi
+
+
+
+def bmi_category(state:bmi_details)->bmi_details:
+    bmi = state['bmi']
+
+    if bmi < 18.5:
+        state["category"] = "Underweight"
+    elif 18.5 <= bmi < 25:
+        state["category"] = "Normal"
+    elif 25 <= bmi < 30:
+        state["category"] = "Overweight"
+    else:
+        state["category"] = "Obese"
+
+    return state
+
+
+
+
+#execution
+
+Grpah=StateGraph(bmi_details)
+
+
+#Nodes
+Grpah.add_node('main',main)
+Grpah.add_node('bmi_category',bmi_category)
+
+#Edges
+Grpah.add_edge(START,'main')
+Grpah.add_edge('main','bmi_category')
+Grpah.add_edge('bmi_category',END)
+#compile
+workflow=Grpah.compile()
+
+
+inputs={'weight':105.5,'height':190}
+
+result=workflow.invoke(inputs)
+
+print(result)
+
